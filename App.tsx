3226b09eeef5b6e83f0bd74fb3b23457882b7d49
stepanturchenko <stepanstepan4@gmail.com>
2021-02-06 21:02:06 +0200

    add copy example from webrtc

--- App.tsx
+++ App.tsx
@@ -1,19 +1,67 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Button, SafeAreaView, StatusBar, StyleSheet, View } from "react-native";
+import { mediaDevices, RTCView } from "react-native-webrtc";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
-export default function App() {
+const App = () => {
+  const [stream, setStream] = useState<any>(null);
+  const start = async () => {
+    console.log('start');
+    const devices = await mediaDevices.enumerateDevices();
+    console.log(devices);
+    if (!stream) {
+      let s;
+      try {
+        s = await mediaDevices.getUserMedia({ video: true });
+        setStream(s);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  };
+  const stop = () => {
+    console.log('stop');
+    if (stream) {
+      stream.release();
+      setStream(null);
+    }
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.body}>
+        {
+          stream &&
+          <RTCView
+            streamURL={stream.toURL()}
+            style={styles.stream} />
+        }
+        <View
+          style={styles.footer}>
+          <Button
+            title="Start"
+            onPress={start} />
+          <Button
+            title="Stop"
+            onPress={stop} />
+        </View>
+      </SafeAreaView>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  body: StyleSheet.absoluteFillObject,
+  stream: {
+    flex: 1
+  },
+  footer: {
+    backgroundColor: Colors.lighter,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0
   },
 });
+
+export default App;
